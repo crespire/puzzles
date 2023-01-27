dea0051a15a54c521e60dc558e1fab5aa1d21268
crespire <li.simmon@gmail.com>
2023-01-27 16:15:07 -0500

    feat: add balanced_array puzzle

---
+++ random/balanced_array.rb
@@ -0,0 +1,18 @@
+# For a given array, does the array contain the same number of evens and odds?
+
+def balanced_array?(arr)
+  return false if arr.length.odd?
+
+  balance = 0
+  arr.each do |number|
+    balance = number.even? ? balance + 1 : balance - 1
+  end
+  balance.zero?
+end
+
+p balanced_array?([1, 2])
+p balanced_array?([1, 1])
+p balanced_array?([])
+p balanced_array?([1])
+p balanced_array?([2])
+p balanced_array?(Array.new(16) { rand(1000) })
